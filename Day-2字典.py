83ec7de56ad9ae5144e702c96917f06a3a59b192
kuoshane <77949544+kuoshane@users.noreply.github.com>
2021-01-26 17:02:25 +0800

    Add files via upload

---
+++ Day-2字典.py
@@ -0,0 +1,5 @@
+dict1 ={"a":"apple","b":"banana","c":"cat"}
+dict1['d'] = 'dog'
+for k,v in dict1.items():
+    print(k,"=>",v)
+
---
+++ Day-2成績表.py
@@ -0,0 +1,14 @@
+score_list=[]
+i=1
+numbers=int(input("人數?"))
+for i in range(numbers):
+    score=int(input("score?"))
+    if score<=60:
+        score=60
+    score_list.append(score)
+    i+=1
+        if i == numbers:
+            score_list.sort()
+            print("全班最高分:",score_list[len(score_list)-1],"分")
+            print("全班最低分:",score_list[len(score_list)-1],"分")
+          
