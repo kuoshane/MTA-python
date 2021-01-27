1cbc07484c29771f71e1c751fff89adf66da21ff
kuoshane <77949544+kuoshane@users.noreply.github.com>
2021-01-27 16:13:24 +0800

    Add files via upload

---
+++ Day-3捕捉兩數相除發生的錯誤.py
@@ -0,0 +1,16 @@
+try:    
+    a=int(input('請輸入被除數'))
+    if a<0:
+        print('請輸入正整數')
+    else:
+        b=int(input('輸入除數'))
+        if b<0:
+            print('請輸入正整數')
+        else:
+            r=a/b
+            print(a,'除以',b,'=',r)
+except ValueError:
+    print('輸入了非數字的值')
+except ZeroDivisionError:
+    print('除數不可為0')
+
